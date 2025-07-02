4cd58030ace134d36eb782a09dd389ede1c98dbe
Miguel <miguelmg.anjo@gmail.com>
2025-07-01 23:17:03 -0300

    Projeto Final

--- script.js
+++ script.js
@@ -41,3 +41,64 @@ document.addEventListener('DOMContentLoaded', function() {
 
     xhr.send();
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById('submitPerfil');
+  const audio = document.getElementById('audioClique');
+
+  botao.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const nome = document.formulario.nome.value.trim();
+    const fone = document.formulario.fone.value.trim();
+    const email = document.formulario.mail.value.trim();
+    const senha = document.formulario.senha.value.trim();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const foneRegex = /^\(\d{2}\) 9\d{4}-\d{4}$/;
+
+    if (!nome) {
+      alert("Por favor, preencha o nome.");
+      return;
+    }
+
+    if (!fone) {
+      alert("Por favor, preencha o telefone.");
+      return;
+    }
+
+    if (!foneRegex.test(fone)) {
+      alert("Por favor, insira o telefone no formato correto: (XX) 9XXXX-XXXX");
+      return;
+    }
+
+    if (!email) {
+      alert("Por favor, preencha o e-mail.");
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      alert("Por favor, insira um e-mail válido.");
+      return;
+    }
+
+    if (!senha) {
+      alert("Por favor, preencha a senha.");
+      return;
+    }
+
+    if (senha.length < 8) {
+      alert("A senha deve conter pelo menos 8 caracteres.");
+      return;
+    }
+
+    alert(`Perfil criado com sucesso!!!\n
+Nome: ${nome}
+Telefone: ${fone}
+E-mail: ${email}`);
+    audio.play();
+  });
+});
+
